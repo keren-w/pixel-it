f51c9b1c525f13c9cbff363584e5b8ac0372813d
keren <keren.w@gmail.com>
2020-08-07 10:52:50 +0300

    Rendering the image on canvas on each file change

--- src/modules/PhotoViewer/components/PhotoViewer.js
+++ src/modules/PhotoViewer/components/PhotoViewer.js
@@ -1,14 +1,67 @@
-import React from 'react';
+import React, {useRef, useEffect} from 'react';
 import styled from "styled-components";
 import Pixelizer from "../../Pixelizer/components/Pixelizer";
 
+const imgSize = {
+		height: 100,
+		width: 100
+}
+
 const PhotoViewer = (props) => {
-		// const {name, file} = props;
-		// console.log(file);
+		const {file} = props;
+		console.log("PhotoViewer -> file", file)
+		const canvasRef = useRef(null);
+
+		useEffect(() => {
+				const canvasElement = canvasRef && canvasRef.current;
+				console.dir(canvasElement)
+				const ctx = canvasElement && canvasElement.getContext('2d');
+				if (file && ctx) {
+						createImageBitmap(file, {
+								// resizeWidth: imgSize.height, resizeHeight: imgSize.width,
+								resizeQuality: 'high'
+						}).then(bitmapImg => {
+								console.log("-> img", bitmapImg)
+								const {height, width} = getDisplayedImageSize(bitmapImg, canvasElement);
+                console.log("PhotoViewer -> height, width", height, width)
+								ctx.drawImage(bitmapImg, 0, 0, width, height);
+						})
+				}
+		}, [file]);
+
+		const getDisplayedImageSize = (bitmapImg, canvasElement) => {
+			const {width, height} = bitmapImg;
+			const isPortaitOrientation = height >= width;
+
+			const canvasHeight = canvasElement.offsetHeight;
+			const canvasWidth = canvasElement.offsetWidth;
+
+			const scaledImageHeight = (canvasWidth/width)*height
+			const scaledimageWidth = (canvasHeight/height)*width
+			
+			const calculatedPortraitSize = {
+				height: canvasHeight,
+				width: scaledimageWidth
+			}
+
+			const calculatedLandscapeSize = {
+				height: scaledImageHeight,
+				width: canvasWidth
+			}
+
+			if (isPortaitOrientation && scaledimageWidth > canvasWidth) {
+				return calculatedLandscapeSize;
+			}
+
+			if (!isPortaitOrientation && scaledImageHeight > canvasHeight) {
+				return calculatedPortraitSize;
+			}
+			return (isPortaitOrientation && calculatedPortraitSize) || calculatedLandscapeSize;
+		}
+
 		return (
 				<ViewerWrapper>
-						<Pixelizer/>
-						<canvas className="my-canvas"/>
+						<canvas ref={canvasRef}/> {file && <Pixelizer/>}
 				</ViewerWrapper>
 		)
 }
--- src/modules/Pixelizer/components/Pixelizer.js
+++ src/modules/Pixelizer/components/Pixelizer.js
@@ -1,15 +1,24 @@
-import React from 'react';
+import React, {useState} from 'react';
 import styled from "styled-components";
 import {ButtonWrapper} from "../../common/styles";
 
 const Pixelizer = (props) => {
+		const [isActive,
+				setIsActive] = useState(false);
 		return (
-				<ButtonWrapper>
-						<div className="container">
+				<Wrapper>
+						<ButtonWrapper>
 								<button className="pixelizer">Pixel it!</button>
-						</div>
-				</ButtonWrapper>
+						</ButtonWrapper>
+				</Wrapper>
 		)
 }
 
 export default Pixelizer;
+
+const Wrapper = styled.div `
+	width: 100%;
+	display: flex;
+	flex-direction: column;
+	align-items: center;
+`;
