3377c0053f284a85059e05e4e2d6a5668f0694fd
keren <keren.w@gmail.com>
2020-08-23 14:07:23 +0300

    TOP_LIMIT_SIZE

--- src/modules/PhotoUploader/data/validator.js
+++ src/modules/PhotoUploader/data/validator.js
@@ -1,3 +1,5 @@
+const TOP_LIMIT_SIZE = 2097152;
+
 export const getPhotoInputValidationResult = (file, accept) => {
     const {type, size} = file;
 
@@ -6,7 +8,7 @@ export const getPhotoInputValidationResult = (file, accept) => {
         return {isValid: false, message: `Please select only files of type ${accept}`}
     }
 
-    if (size && size > 2097152) {
+    if (size && size > TOP_LIMIT_SIZE) {
         // throw error large file
         return {isValid: false, message: 'please upload a file below 2 MB'};
     }
