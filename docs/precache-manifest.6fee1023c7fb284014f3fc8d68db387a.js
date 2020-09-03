44ab8def8dd260c1483ce225747f32a4a3df10c4
keren <keren.w@gmail.com>
2020-09-03 10:21:56 +0300

    build docs lib for github pages

---
+++ docs/precache-manifest.6fee1023c7fb284014f3fc8d68db387a.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "e86a473b0f6166f0e24037f3b5f6ff83",
+    "url": "/static/media/upload.e86a473b.svg"
+  },
+  {
+    "revision": "7c4eb3b94ebfea015f6cff2051a0932f",
+    "url": "/static/media/upload-icon.7c4eb3b9.svg"
+  },
+  {
+    "revision": "f3bf0b34e1effba2fc7384fcb963d253",
+    "url": "/static/media/Pixelit_svg.f3bf0b34.svg"
+  },
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.fdfcfda2.js"
+  },
+  {
+    "revision": "65e076e9d8705b801b3d",
+    "url": "/static/js/main.65e076e9.chunk.js"
+  },
+  {
+    "revision": "a125507af5a2465c6896",
+    "url": "/static/js/2.a125507a.chunk.js"
+  },
+  {
+    "revision": "65e076e9d8705b801b3d",
+    "url": "/static/css/main.ca472b03.chunk.css"
+  },
+  {
+    "revision": "c85e514f25037bbb70389a365bc398ea",
+    "url": "/index.html"
+  }
+];
