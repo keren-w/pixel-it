c44b9d0ce3500c544de20d0c8a518cb223fd0d1f
keren <keren.w@gmail.com>
2020-09-06 13:52:08 +0300

    build

---
+++ docs/precache-manifest.c98b1f3bc9d92f5e9d766152882b63ee.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "e86a473b0f6166f0e24037f3b5f6ff83",
+    "url": "./static/media/upload.e86a473b.svg"
+  },
+  {
+    "revision": "7c4eb3b94ebfea015f6cff2051a0932f",
+    "url": "./static/media/upload-icon.7c4eb3b9.svg"
+  },
+  {
+    "revision": "f3bf0b34e1effba2fc7384fcb963d253",
+    "url": "./static/media/Pixelit_svg.f3bf0b34.svg"
+  },
+  {
+    "revision": "9eb600ee07a27cdad64f",
+    "url": "./static/js/runtime~main.9eb600ee.js"
+  },
+  {
+    "revision": "72b848e48794e906237f",
+    "url": "./static/js/main.72b848e4.chunk.js"
+  },
+  {
+    "revision": "b188769aa112ca31245d",
+    "url": "./static/js/2.b188769a.chunk.js"
+  },
+  {
+    "revision": "72b848e48794e906237f",
+    "url": "./static/css/main.d07a9cf3.chunk.css"
+  },
+  {
+    "revision": "181d2a293da06c72f016660fb6060fd0",
+    "url": "./index.html"
+  }
+];
