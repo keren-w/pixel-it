8553404add1f800c62ad776c500ccdd141f43663
keren <keren.w@gmail.com>
2020-09-06 17:32:49 +0300

    slider arrow location

--- src/modules/ImageView/components/Slider.js
+++ src/modules/ImageView/components/Slider.js
@@ -6,7 +6,7 @@ const SLIDER_BORDER_SIZE = 1;
 const SLIDER_HYPOTENUSE = Math.sqrt(Math.pow(SLIDER_ARROW_SIZE * 2, 2) * 2);
 
 const Slider = (props) => {
-const {onMouseDown, forwardRef} = props;
+    const {onMouseDown, forwardRef} = props;
     return (
         <SliderWrapper
             draggable
@@ -40,15 +40,15 @@ const Arrow = styled.span `
     width: 100%;
     border: ${SLIDER_BORDER_SIZE}px solid white;
     position: absolute;
-    top: ${props => `${ (props.theme.imageMeasures.height / 2 - SLIDER_HYPOTENUSE / 2 + 3)}px`}; /* offset between two elements*/
+    top: ${props => `${(props.theme.imageMeasures.height-SLIDER_HYPOTENUSE)/2 + SLIDER_HYPOTENUSE/2-SLIDER_ARROW_SIZE}px`}; /* offset between two elements*/
     left: 0;
     transform: rotate(45deg);
 `;
 const TopSeperator = styled.div `
     width: ${SLIDER_BORDER_SIZE}px;
     background-color: white;
-    height: ${props => `${ (props.theme.imageMeasures.height / 2 - SLIDER_HYPOTENUSE / 2) - 1}px`};
-    left: ${SLIDER_ARROW_SIZE + 1}px;
+    height: ${props => `${(props.theme.imageMeasures.height-SLIDER_HYPOTENUSE)/2}px`};
+    left: ${SLIDER_ARROW_SIZE - SLIDER_BORDER_SIZE/2}px;
     position: absolute;
     top: 0;
 `;
